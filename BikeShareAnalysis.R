6b5cc63c98d2096aac1b159070f4db6b7925b381
Zach Wooten <zacharyashtonwooten@gmail.com>
2023-09-29 13:43:53 -0600

    random forest

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -232,3 +232,66 @@ bike_preds_tree <- final_wf2 %>%
   mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
 
 vroom_write(x=bike_preds_tree, file="./BikePredsTree.csv", delim=",")
+
+
+## random forest
+
+my_mod4 <- rand_forest(mtry = tune(),
+                      min_n = tune(),
+                      trees = 500) %>% #Type of model
+  set_engine("ranger") %>% # what r function to use
+  set_mode("regression")
+
+# Create a workflow with model and recipe
+my_recipe3 <- recipe(count~., data = logTrainSet) %>%
+  # make these variables factors
+  step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Relabel weather 4 to 3
+  step_mutate(weather=factor(weather, levels=1:3, labels=c("Sunny", "Mist", "Rain"))) %>%
+  step_mutate(season=factor(season, levels=1:4, labels=c("Spring", "Summer", "Fall", "Winter"))) %>%
+  step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_time(datetime,features = "hour") %>% # add hour variable
+  # change name of variable
+  step_rename(hour = datetime_hour) %>%
+  step_rm(datetime) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_numeric_predictors())
+
+
+wf3 <- workflow() %>%
+  add_recipe(my_recipe3) %>%
+  add_model(my_mod4)
+
+# Set up grid of tuning values
+tuning_grid3 <- grid_regular(mtry(range = c(1,10)),
+                             min_n(),
+                             levels = 5)
+
+# Set up K-fold CV
+folds <- vfold_cv(logTrainSet, v = 5, repeats = 1)
+# Run the CV
+CV_results3 <- wf3 %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid3,
+            metrics = metric_set(rmse, mae, rsq))
+
+# Find best tuning parameters
+bestTune3 <- CV_results3 %>%
+  select_best("rmse")
+
+# Finalize workflow and predict
+final_wf3 <- 
+  wf3 %>%
+  finalize_workflow(bestTune3) %>%
+  fit(data = logTrainSet)
+
+bike_preds_forest <- final_wf3 %>%
+  predict(new_data = bike.test) %>%
+  mutate(.pred=exp(.pred)) %>% # Back-transform the log to original scale
+  bind_cols(., bike.test) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and predictions
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+vroom_write(x=bike_preds_forest, file="./BikePredsForest.csv", delim=",")
